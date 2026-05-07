785dc7f7fa56ae89850b4e4b602f381af7dfc181
Connor Bailey <connorwb2005@gmail.com>
2026-05-06 22:54:02 -0500

    Add GuildLog

--- enchanted-courier-guild/src/main/java/edu/oop/guild/log/GuildLog.java
+++ enchanted-courier-guild/src/main/java/edu/oop/guild/log/GuildLog.java
@@ -1,5 +1,39 @@
 package edu.oop.guild.log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuildLog {
 
+	private static GuildLog instance;
+	
+	private final List<String> log = new ArrayList<>();
+	
+	private GuildLog() {}
+	
+	public static GuildLog getInstance() {
+		if(instance == null) {
+			instance = new GuildLog();
+		}
+		return instance;
+	}
+	
+	public void record(String entry) {
+		if(entry == null) {
+			throw new NullPointerException("Cannot input null string.");
+		}
+		log.add(entry);
+	}
+	
+	public void clear() {
+		log.clear();
+	}
+	
+	public int size() {
+		return log.size();
+	}
+	
+	public List<String> entries() {
+		return List.copyOf(this.log);
+	}
 }
